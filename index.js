e04a134615735f9b79071f99d720b1c94fd0ed33
samowen62 <sam@hardindd.com>
2015-09-01 19:11:57 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+app.get('/three.js', function(req, res){
+  res.sendfile('three.js');
+});
+
+app.get('/three.min.js', function(req, res){
+  res.sendfile('three.min.js');
+});
+
+app.get('/js/renderers/Projector.js', function(req, res){
+  res.sendfile('js/renderers/Projector.js');
+});
+
+app.get('/js/renderers/CanvasRenderer.js', function(req, res){
+  res.sendfile('js/renderers/CanvasRenderer.js');
+});
+
+app.get('/js/libs/stats.min.js', function(req, res){
+  res.sendfile('js/libs/stats.min.js');
+});
+
+//io.on('connection', function(socket){
+//  console.log('a user connected');
+//});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
+
