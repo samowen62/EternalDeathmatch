bc40aef2d67978e89be7490d83e320513562096a
samowen62 <samowen62@yahoo.com>
2015-10-09 13:05:47 -0500

    added platform class

--- user/objects.js
+++ user/objects.js
@@ -44,3 +44,37 @@ var collisionWall = class {
     return 0
    }
 };
+
+
+var platform = class {
+  //need plane normal and d s.t. the plane is {x : n * x = d}
+  //specify verticies of three pts
+  //variable on player whether grounded or not for physics
+
+  /*
+   * constructor takes array of 3 Vector3's
+   */
+  constructor(points) {
+    if(points.length != 3)
+      return
+
+    var tmp1 = new THREE.Vector3(),tmp2 = new THREE.Vector3(),tmp3 = new THREE.Vector3();
+
+    tmp1.subVectors(points[0], points[1])
+    tmp2.subVectors(points[0], points[2])
+    tmp3.crossVectors(tmp2, tmp1)
+    tmp3.normalize()
+
+    if(tmp3.y < 0)
+      tmp3.multiplyScalar(-1.0)
+
+    this.normal = tmp3, this.d = points[0].dot(tmp3)
+  }
+
+  //not really above, but tells which side of the plane it's on, 
+  //maybe subtract a little from d and see if the point is above the this.HIGHEST_Y_COORD
+  above(point){
+    return point.dot(this.normal) > this.d
+  }
+
+};
