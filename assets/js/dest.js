dcec4f950ec40f5e492ad66a6ae1ee5567f104b9
samowen62 <samowen62@yahoo.com>
2015-10-18 21:54:34 -0500

    jumping bug fixed

--- assets/js/dest.js
+++ assets/js/dest.js
@@ -1830,6 +1830,7 @@ var collisionWall = class {
     this.center = tmp1
 
     tmp2.subVectors(upperLeft, lowerRight)
+    this.halfHeight = upperLeft.y - lowerRight.y
     lowerRight.y = upperLeft.y
     tmp3.subVectors(upperLeft, lowerRight)
     tmp3.crossVectors(tmp2, tmp3)
@@ -1837,6 +1838,7 @@ var collisionWall = class {
 
     this.normal = tmp3
     this.next = null,this.prev = null
+    
 
     lowerRight.y = ly
     this.verts = {ul : upperLeft, lr : lowerRight}
@@ -1872,11 +1874,20 @@ var collisionWall = class {
   /*
 	 * returns positive values if 'inside' (side normal points to),
 	 * negative values if on the outside, and 0 if point is inline
-	 * with this boundary wall
+	 * with this boundary wall that extends to infinity in y
    */
-  whichSide(point){
+  whichSideAbsolute(point){
   	calcVec.subVectors(point,this.center)
   	return calcVec.dot(this.normal);// > 0 ? 'inside' : 'outside';
+  }
+
+  //will return 0 if the point is above or below the wall
+  whichSide(point){
+    calcVec.subVectors(point,this.center)
+      if(point.y > this.center.y + this.halfHeight || point.y < this.center.y - this.halfHeight ){
+        return 0;
+      }
+    return calcVec.dot(this.normal);// > 0 ? 'inside' : 'outside';
   }
 
   //statics take 2 objs as input
@@ -1896,7 +1907,19 @@ var platform = class {
    */
   constructor(points) {
     if(points.length != 3)
-      return
+      return;
+
+    this.points = points;
+//    this.points = [new THREE.Vector2(points[0].x, points[0].z),new THREE.Vector2(points[1].x, points[1].z),new THREE.Vector2(points[2].x, points[2].z)]
+    //for barycentric coordinates
+    this.v0 = new THREE.Vector3(points[1].x - points[0].x,points[1].y - points[0].y,points[1].z - points[0].z);
+    this.v1 = new THREE.Vector3(points[2].x - points[0].x,points[2].y - points[0].y,points[2].z - points[0].z);
+    this.d00 = this.v0.dot(this.v0);
+    this.d01 = this.v1.dot(this.v0);
+    this.d11 = this.v1.dot(this.v1);
+    this.den = this.d00 * this.d11 - this.d01 * this.d01;
+    if(this.den == 0)
+      this.den = 1;//invalid
 
     var tmp1 = new THREE.Vector3(),tmp2 = new THREE.Vector3(),tmp3 = new THREE.Vector3();
 
@@ -1910,6 +1933,12 @@ var platform = class {
     this.normal = tmp3, this.d = points[0].dot(tmp3)
   }
 
+  //maybe just calculate barycentric coords
+  sign (p1,p2,p3)
+  {
+    return (p1.x - p3.x) * (p2.z - p3.z) - (p2.x - p3.x) * (p1.z - p3.z);
+  }
+
   //not really above, but tells which side of the plane it's on, 
   //maybe subtract a little from d and see if the point is above the this.HIGHEST_Y_COORD
   //might want to call below to make more clear?
@@ -1917,7 +1946,38 @@ var platform = class {
   //WONT WORK FOR SLANTED HILL PLATFORMS SINCE THE PLANE TAKES UP THE ENTIRE ENVIREONMENT
   //SO ONLY LOOK AT IT IF THE USER IS OVER IT
   above(point){
-    return point.dot(this.normal) > this.d
+    return point.dot(this.normal) > this.d;
+  }
+
+  /*
+   * Checks if we are even within the x/z triangle in question
+   */
+  over(point){
+    var pt = new THREE.Vector2(point.x, point.z),
+        b1 = this.sign(pt, this.points[0], this.points[1]) < 0,
+        b2 = this.sign(pt, this.points[1], this.points[2]) < 0,
+        b3 = this.sign(pt, this.points[2], this.points[0]) < 0;
+
+    return ((b1 == b2) && (b2 == b3));
+  }
+
+  baryCoords(pos){
+    var v2 = new THREE.Vector3(0,0,0);
+    v2.subVectors(pos,this.points[0]);
+
+    var d20 = v2.dot(this.v0),
+        d21 = v2.dot(this.v1);
+        
+    //u,v bary for points 0 and 1
+    var v = (this.d11 * d20 - this.d01 * d21) / this.den,
+        u = 1 - v - ((this.d00 * d21 - this.d01 * d20) / this.den);
+
+    return u * this.points[0].y + v * this.points[1].y;
+
+  }
+
+  yAt(pos){
+    return this.baryCoords(pos);
   }
 
 };
@@ -1967,33 +2027,40 @@ var cEntity = class {
     
 
     if(Controller.keyIsDown[32] && this.grounded){
+      console.log('jumping')
       var d = new Date();
       this.grounded = false;
-      this.air_v = 60;
+      //this.air_v = 60;
+      this.air_v = 100;
       this.start_t = d.getTime();
       this.air_o = this.position.y;
       s *= 0.4;
     }
 
+    //check for landing and clunking head by looping through platforms
     if(!this.grounded){
       var d = new Date();
-      var dt = (d.getTime() - this.start_t) / 100; 
-      //check for landing and clunking head by looping through platforms
+      var dt = (d.getTime() - this.start_t) / 100;       
       var new_y = -5 * (dt*dt) + this.air_v*dt + this.air_o
 
+
       //need to add character y thickness
-      var new_pos = new THREE.Vector3(this.position.x, new_y - this.thickness.y, this.position.z);
+      // or not?
+      var new_pos = new THREE.Vector3(this.position.x, new_y, this.position.z);
       for(var g in ground){
-        var a = ground[g].above(this.position), b = ground[g].above(new_pos)
-        if((!a && b)||(a && !b)){
-          console.log('landed')
-          this.grounded = true;
-        }
-        else{
-          this.position.y = new_y
+        if(ground[g].over(this.position)){
+          var a = ground[g].above(this.position), b = ground[g].above(new_pos);
+          if((!a && b)||(a && !b)){
+            this.grounded = true;
+            break;
+          }
         }
       }
+
+      if(!this.grounded)
+        this.position.y = new_y;
     }
+
 
     tmpVec.copy(this.position);
     tmpPos.copy(this.position);
@@ -2072,15 +2139,24 @@ var cEntity = class {
         }
       }
     
+    //if grounded look at the platform we are over in the future and calculate the y value
     tmpVec.y = tY;
     tmpVec.copy(this.detectCol(tmpPos,tmpVec));
 
+    if(this.grounded){
+      for(var g in ground){
+        if(ground[g].over(this.position)){
+          tmpVec.y = ground[g].yAt(this.position);// + this.thickness.y; //for some reason crashes?
+          break;
+        }
+      }
+    }
+
+    tmpVec.y = tmpVec.y + this.thickness.y;
     //console.log(tmpVec)
     //special camera effects could go here
     
     this.position.copy(tmpVec);
-
-
   }
 
   detectCol(present,future){
@@ -2089,7 +2165,7 @@ var cEntity = class {
 
     //must detect ground clipping here
     //only or all in ground if on HORIZON
-    if(!ground[0].above(this.position) || !ground[1].above(this.position)) console.log('underground')
+    //if(!ground[0].above(this.position) || !ground[1].above(this.position)) console.log('underground')
 
     var wxMax,wxMin,wzMax,wzMin,tiles = [],t = 2*sqThick,a,b
 
