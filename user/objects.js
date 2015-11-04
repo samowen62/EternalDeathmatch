a20a8ae50477988866ef5790c3c014bca644878e
samowen62 <samowen62@yahoo.com>
2015-11-04 12:45:04 -0600

    added ramp

--- user/objects.js
+++ user/objects.js
@@ -253,6 +253,98 @@ var platform = class {
 
 };
 
+//var points = [new THREE.Vector3(2,10,4), new THREE.Vector3(5,10,0), new THREE.Vector3(1,0,-3), new THREE.Vector3(-2,0,1)]
+//like collision wall but up is in x/y plane and walkable
+var ramp = class {
+  /*
+   * input in clockwise order
+   * first and last two should have same y coordinate
+  */
+  constructor(points) {
+    if(points.length != 4)
+      return;
+    if(points[0].y != points[1].y || points[2].y != points[3].y)
+      return;
+    this.points = points;
+
+    var tmp1 = new THREE.Vector3(),tmp2 = new THREE.Vector3(),tmp3 = new THREE.Vector3();
+
+    tmp1.subVectors(points[0], points[1]);
+    tmp2.subVectors(points[0], points[2]);
+    tmp3.crossVectors(tmp2, tmp1);
+    tmp3.normalize();
+    if(tmp3.y < 0) tmp3.multiplyScalar(-1.0);
+    this.normal = tmp3, this.d = points[0].dot(tmp3);
+
+    tmp1.addVectors(points[0],points[1]);
+    tmp2.addVectors(points[2], points[3]);
+    tmp1.add(tmp2);
+    tmp1.multiplyScalar(0.25);
+    this.center = tmp1;//average of all points
+
+    //assume first two points are largest for now
+    tmp1 = new THREE.Vector3((points[2].x+points[3].x)*0.5 - this.center.x,points[2].y - this.center.y,(points[2].z+points[3].z)*0.5 - this.center.z)
+    tmp1.normalize();
+    this.down_ramp = tmp1;
+    this.side_ramp = new THREE.Vector3(-this.down_ramp.z, 0,this.down_ramp.x);//90 deg ccw (+90)
+    //down/side and normal make ortho basis for this ramp
+
+    this.len_across = Math.pow(points[0].x-points[1].x,2)+Math.pow(points[0].z-points[1].z,2);
+    this.len_down = Math.pow(points[0].x-points[3].x,2)+Math.pow(points[0].z-points[3].z,2);
+  
+  }
+
+  above(point){
+    return point.dot(this.normal) >= this.d;
+  }
+
+  /*
+   * Checks if we are even within the x/z triangle in question
+   * and returns appropriate y
+   */
+  over(point){
+    //might actually work
+    var tmp2Vec = new THREE.Vector2(point.x-this.center.x, point.z-this.center.z); //put point relative to center
+
+    var p_down = tmp2Vec.dot(this.down_ramp);//component down
+    if(Math.pow(p_down,2) > this.len_down)
+      return false;
+
+    var p_side = tmp2Vec.dot(this.side_ramp);//component down
+    if(Math.pow(p_side,2) > this.len_across)
+      return false;
+
+    var y = this.center.y + p_down * this.down_ramp.y;//need actual 3d y component for accuracy
+    console.log(y);
+    return y;
+
+  }
+
+  render(){
+    var geometry = new THREE.Geometry();
+
+    geometry.vertices.push( this.points[0]);
+    geometry.vertices.push( this.points[1]);
+    geometry.vertices.push( this.points[2]);
+    geometry.vertices.push( this.points[3]);
+
+    geometry.faces.push( new THREE.Face3( 0, 1, 2 ) );
+    geometry.faces.push( new THREE.Face3( 0, 2, 3 ) );
+
+    geometry.computeFaceNormals();
+    geometry.computeVertexNormals();
+
+    return geometry;
+  }
+
+  yAt(pos){
+    
+  }
+
+};
+
+
+
 var ceiling = class {
   
   constructor(points) {
