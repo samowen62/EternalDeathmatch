df268cc3b20da7d206d8bbc1f5126ce3e4a367bd
Sam Owen <samowen62@yahoo.com>
2015-12-18 17:19:54 -0600

    rooms adjusted

--- index.js
+++ index.js
@@ -1,80 +1,121 @@
+var num_rooms = 2;
+var room_size = 2;
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var rooms = ['lobby'], newId
+var rooms = [], newId
 var BASE_LATENCY = 5 
 
+
+for(var i = 0; i < num_rooms; i++){
+  //things we want to store on a per room basis
+  rooms.push({
+    id : makeid(8),
+    members : {},
+    host : ''
+
+  });
+}
+
 //need hashes for user and game
-function makeid(){
+function makeid(len){
   var text = "",possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for( var i=0; i < 25; i++ )
+  for( var i=0; i < len; i++ )
       text += possible.charAt(Math.floor(Math.random() * possible.length));
   return text;
 }
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendfile( __dirname + '/index.html');
 });
 
 app.get('/assets/css/*', function(req, res){
-  console.log( __dirname + req.originalUrl)
-  res.sendfile( __dirname + req.originalUrl);
+  res.sendFile( __dirname + '/' + req.originalUrl);
 });
 
 app.get('/images/*', function(req, res){
-  console.log( __dirname + req.originalUrl)
-  res.sendfile( __dirname + req.originalUrl);
+  res.sendFile( __dirname + '/' + req.originalUrl);
 });
 
 app.get('/sounds/*', function(req, res){
-  console.log( __dirname + req.originalUrl)
-  res.sendfile( __dirname + req.originalUrl);
+  res.sendFile( __dirname + '/' + req.originalUrl);
 });
 
 app.get('/app.js', function(req, res){
-  res.sendfile('assets/js/dest.js');
+  res.sendFile(__dirname +  '/assets/js/dest.js');
 });
 app.get('/jquery.js', function(req, res){
-  res.sendfile('assets/js/jquery-1.11.1.js');
+  res.sendFile(__dirname +  '/assets/js/jquery-1.11.1.js');
 });
 app.get('/socket.js', function(req, res){
-  res.sendfile('assets/js/socket.io-1.2.0.js');
+  res.sendFile( __dirname + '/assets/js/socket.io-1.2.0.js');
 });
 
 io.sockets.on('connection', function(socket){
-  newId = makeid()
+
+  newId = makeid(25);
+  socket.uniq_id = newId;
 
   //transmit initial data to client
+  //might not need
   socket.emit('id', { 
     id : newId
   });
 
-  socket.room = rooms[0]//random room later
-  socket.iter = 0
-  socket.latency = BASE_LATENCY
-  socket.join(socket.room)
+  //need to change this so that we respect room size
+  var choice = Math.floor(num_rooms*Math.random());
+  socket.room_id = choice;
+
+
+  socket.iter = 0;
+  socket.latency = BASE_LATENCY;
+
+  socket.room = rooms[socket.room_id].id;
+  socket.join(socket.room);
+ 
 
   socket.on('disconnect', function() {
     console.log('User disconnected');
-    socket.leave(socket.room)
+    
+    delete rooms[socket.room_id].members[socket.uniq_id];
+
+    if(rooms[socket.room_id].host == socket.uniq_id){
+      var keys = Object.keys(rooms[socket.room_id].members);
+      rooms[socket.room_id].host = keys.length == 0 ? '' : keys[0];
+    }
+    
+    console.log(rooms);
+    //could dynamically alter who is host based on latency
+    socket.leave(socket.room_id);
   });
 
   console.log('user connected');
-  socket.on('m', function(msg){
-    socket.iter = socket.iter + 1
 
+  if(rooms[socket.room_id].host == '')
+    rooms[socket.room_id].host = newId;
+  rooms[socket.room_id].members[newId] = {};
+
+  console.log(rooms);
+
+  socket.on('m', function(msg){
+    socket.iter = socket.iter + 1;
+
+    rooms[socket.room_id].members[socket.uniq_id]['msg'] = msg;
+    //console.log(socket.room.members, socket.room.host);
+    
     /*
       optimize per socket latency later to respond more frequently
       to faster clients that demand more and vice-versa
     */
-    if(socket.iter == socket.latency){
+    /*if(socket.iter == socket.latency){
       socket.iter = 0;
       socket.broadcast.to(socket.room).emit('o', { 
         x : msg.x,
         y : msg.y,
         z : msg.z
       });
-    }
+    }*/
     //broadcast.to doesn't emit to sender while
     //io.sockets.in(room).emit() emits to all
     
